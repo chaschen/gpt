5a8f18193fb3b2a90a7f8b6a77a23ca83671d25d
chaschen <128336914+chaschen@users.noreply.github.com>
2023-03-23 19:22:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 import streamlit as st 
-import pandas
+import openai
 
 st.write("its my first App")
+
+def show_message(text):
+  messages_str = [
+      f"{_['role']}: {_['content']}" for _ in st.session_state["messages"][1:]
+  ]
+  text.text_area("messages", value = str("\n\n".join(messages_str)), height=400 )
+  
+  
+ BASE_PROMPT = [{"role":"system", "content":"your assistant."}]
+
+st.subheader("ChatGpt from Chaschen")
+
+text = st.empty()
+show_message(text)
